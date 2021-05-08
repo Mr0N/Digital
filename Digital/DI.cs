ec2bd3db95e10c6137cbf34350e3511457d5b0b2
new_user <new_user@gmail.com>
2021-05-08 21:31:27 +0300

    Добавьте файлы проекта.

---
+++ Digital/DI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+
+namespace Digital
+{
+
+    public class DI
+    {
+        public void Set<Interface,Q>() where Q:Interface,new()
+        {
+            Func<Interface> action;
+            lock (obj) 
+                action = () => new Q();
+            var type = typeof(Interface);
+            if (dict.ContainsKey(type)) dict.Remove(type,out var xxx);
+            while (!dict.TryAdd(type, action as Func<object>)) { }
+        }
+        public Interface Get<Interface>()
+        {
+            if (!dict.TryGetValue(typeof(Interface), out var value))
+                throw new Exception("Type not found");
+            lock(obj)
+            return (value as Func<Interface>)();
+        }
+        ConcurrentDictionary<Type, Func<object>> dict;
+        object obj;
+        public DI()
+        {
+            this.obj = new object();
+            dict = new ConcurrentDictionary<Type, Func<object>>();
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,44 @@
+﻿using Digital;
+using System;
+
+namespace Test
+{
+    interface IXXX
+    {
+        public string Message();
+    }
+    class XXX : IXXX
+    {
+        public string Message()
+        {
+            return "12345678";
+        }
+    }
+
+    interface IYYY
+    {
+        public string Message();
+    }
+    class YYY : IYYY
+    {
+        public string Message()
+        {
+            return "12345678";
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DI di = new DI();
+            di.Set<IXXX, XXX>();
+            Console.WriteLine(di.Get<IXXX>().Message());
+            di.Set<IYYY, YYY>();
+            Console.WriteLine(di.Get<IYYY>().Message());
+            Console.ReadKey();
+            
+        }
+    }
+}
